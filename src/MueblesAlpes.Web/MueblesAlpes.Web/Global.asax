11248c7fe18635d797899725a8fe56deb1f89019
Ulices santos <jsantosc16@miumg.edu.gt>
2026-03-09 18:43:38 -0600

    refactor: clean incorrect folder structure and organize repository properly

---
+++ src/MueblesAlpes.Web/MueblesAlpes.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="MueblesAlpes.Web.Global_asax" Language="vb" %>
---
+++ src/MueblesAlpes.Web/MueblesAlpes.Web/My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("MueblesAlpes.Web")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MueblesAlpes.Web")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("31ef7dd6-e61a-4292-8bc7-07f5b6e2786e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
