8066b5c1bd09beb7c7a29d00b28aa9431f5935be
RobbieP26 <rperezc34@miumg.edu.gt>
2026-05-15 14:56:39 -0600

    Actualizacion Procedimientos BOD_CATEGORIA

--- database/procedures/catalogo_inventario/PKG_BOD_CATEGORIA.pks.sql
+++ database/procedures/catalogo_inventario/PKG_BOD_CATEGORIA.pks.sql
@@ -1,13 +1,11 @@
-CREATE OR REPLACE PACKAGE PKG_BOD_PRODUCTO AS
-    PROCEDURE CREAR(p_referencia IN VARCHAR2, p_nombre IN VARCHAR2, p_descripcion IN VARCHAR2, p_tip_tipo IN NUMBER, p_mat_material IN NUMBER, p_alto_cm IN NUMBER, p_ancho_cm IN NUMBER, p_profundidad_cm IN NUMBER, p_color IN VARCHAR2, p_peso IN NUMBER, p_foto IN BLOB);
-    PROCEDURE ACTUALIZAR(p_referencia IN VARCHAR2, p_nombre IN VARCHAR2, p_descripcion IN VARCHAR2, p_tip_tipo IN NUMBER, p_mat_material IN NUMBER, p_alto_cm IN NUMBER, p_ancho_cm IN NUMBER, p_profundidad_cm IN NUMBER, p_color IN VARCHAR2, p_peso IN NUMBER);
-    PROCEDURE ACTUALIZAR_FOTO(p_referencia IN VARCHAR2, p_foto IN BLOB);
-    PROCEDURE ACTUALIZAR_PRECIO(p_referencia IN VARCHAR2, p_precio IN NUMBER);
-    PROCEDURE ELIMINAR(p_referencia IN VARCHAR2);
-    PROCEDURE OBTENER(p_referencia IN VARCHAR2, p_data OUT SYS_REFCURSOR);
-    PROCEDURE LISTAR(p_data OUT SYS_REFCURSOR);
-    PROCEDURE LISTAR_POR_CATEGORIA(p_categoria IN NUMBER, p_data OUT SYS_REFCURSOR);
-    PROCEDURE BUSCAR(p_texto IN VARCHAR2, p_data OUT SYS_REFCURSOR);
-    PROCEDURE OBTENER_FOTO(p_referencia IN VARCHAR2, p_foto OUT BLOB);
-END PKG_BOD_PRODUCTO;
+-- ============================================================
+-- 1. PKG_BOD_CATEGORIA
+-- ============================================================
+CREATE OR REPLACE PACKAGE PKG_BOD_CATEGORIA AS
+  PROCEDURE CREAR(p_descripcion IN VARCHAR2, p_id OUT NUMBER);
+  PROCEDURE ACTUALIZAR(p_id IN NUMBER, p_descripcion IN VARCHAR2);
+  PROCEDURE ELIMINAR(p_id IN NUMBER);
+  PROCEDURE LISTAR(p_data OUT SYS_REFCURSOR);
+  PROCEDURE BUSCAR(p_texto IN VARCHAR2, p_data OUT SYS_REFCURSOR);
+END PKG_BOD_CATEGORIA;
 /
